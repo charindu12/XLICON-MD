f8c99fa95f61b0d51f649c9c30b734200799c91c
charindu12 <charindunuwan5@gmail.com>
2025-05-17 23:37:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,25 +3,25 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 
 //═══════[Required Variables]════════\\
-global.owner = process.env.OWNER_NUMBER || "923184070915"  // Make SURE its Not Be Empty, Else Bot Stoped And Errors,
+global.owner = process.env.OWNER_NUMBER || "94762890901"  // Make SURE its Not Be Empty, Else Bot Stoped And Errors,
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://salmanahmad:s4salmanyt@cluster0.szcj2eo.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
 global.email = 'xheikhsalman4422@gmail.com'
 global.github = 'https://github.com/salmanytofficial/XLICON-MD'
 global.location = 'Lahore Pak'
 global.gurl = 'https://instagram.com/itz_shaikho/' // add your username
-global.sudo = process.env.SUDO || '923184070915' 
+global.sudo = process.env.SUDO || '94762890901' 
 global.devs = '923184070915';
 global.website = 'https://github.com/salmanytofficial/XLICON-MD' //wa.me/+91000000000000
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/3c341828d86ee7a89c73f.jpg'
 module.exports = {
-  sessionName:  process.env.SESSION_ID ||  " ",  //PUT Session Id Here, 
+  sessionName:  process.env.SESSION_ID ||  "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiYUNGOEpQeVlHcXpySng2Qkx0c2FuL2s2azY1Rm16dDVhNHhmT0RkaWtrVT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiL0QxQXgwL3V1MFJwU3R4NmhwSG4wNG9ueGlvTzR2U3Fsb09jV1BCcFF6Yz0ifX0sInNpZ25lZElkZW50aXR5S2V5Ijp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJTUHNVNXFPcmpUV1RNU0tNMTR4alFKcFdtRStPaENpRVkrZ2l2RW9mdUUwPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJjVm1KNjFFNklOQ0tKK2RHQkI2dGRZUGhPZkUxUlo1NFZ1OWFjUjgwL3pFPSJ9fSwic2lnbmVkUHJlS2V5Ijp7ImtleVBhaXIiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Im9IZ0ZHcU1ibzI0ekVCcXlab09WV2pLWHJJZndVc1FlcWJqVVEyMDhPMHc9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InZCd3ZaNHMxaHVaSFBldkVjSnR5S3hLYktzdXRTci83dWc4UzkweTJsMEE9In19LCJzaWduYXR1cmUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ0bTJOMTBnM3haeHJHWHpXZmhFcTVUVFFRTHVNclIvQUZtRjVsOXhXeEtUNmxPWUlRbkNOcGg5WTJIUWRQS2xqaXpqUDNpY3BpSmJLMkd2bkxJcXVEdz09In0sImtleUlkIjoxfSwicmVnaXN0cmF0aW9uSWQiOjM4LCJhZHZTZWNyZXRLZXkiOiJSbTRWYWxLcGVQbUdWYzBPMHRHdnI3NGxqdktHa2g5TWpRQXNyL2tEUEgwPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJsYnpTZDhuOFJwZURCQjFKdlY2dUFRIiwicGhvbmVJZCI6IjFlODNhYmEzLTViODUtNGNlOS1iZjE4LTFkNTQxYTFhOTA2OCIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJSc1A0V0w0Slhqa3ZJejdYK3lqN295VmdVWHM9In0sInJlZ2lzdGVyZWQiOmZhbHNlLCJiYWNrdXBUb2tlbiI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IklFVWtkb21BdGEvS01oRFM1WkttTWRqcWlmdz0ifSwicmVnaXN0cmF0aW9uIjp7fSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ0pMbDZlVUZFSXlMbzhFR0dBRWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6InQwQjhXVGhWUy9DbkhuR3dGMTloOG9ZQk1tM3VHY09HdUpQUEVpcXFSWE09IiwiYWNjb3VudFNpZ25hdHVyZSI6ImtLMVlkaWZzL0hXYU9YQ1ZtVnNpVzhRZEY2N0htNVQ1TCtlck12K0QwMGRzN3ZSMEpJTzU4SWdDUTdCZ1pDL0wyVnZLRXU4L0QzQ1VEeEFFdndrSEJRPT0iLCJkZXZpY2VTaWduYXR1cmUiOiJ6VHVXdUZFWmZoNXo2SllhWjkwSmkyODFZVi9mVWJRVHl5bmgxeHNGbGxwSXlNRGJoeFdKeWxncFdyaDYzS2V5RHM0MFl0QjZ3Ly9lazVScGhFNlhDQT09In0sIm1lIjp7ImlkIjoiOTQ3NjI4OTA5MDE6NzZAcy53aGF0c2FwcC5uZXQifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiOTQ3NjI4OTA5MDE6NzZAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCYmRBZkZrNFZVdndweDV4c0JkZllmS0dBVEp0N2huRGhyaVR6eElxcWtWeiJ9fV0sInBsYXRmb3JtIjoiYW5kcm9pZCIsImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTc0NzUwMjQ3OX0=",  //PUT Session Id Here, 
   author:  process.env.PACK_AUTHER ||  '',
   packname:  process.env.PACK_NAME || '',
    
   botname:   process.env.BOT_NAME === undefined ? "Xlicon-Md" : process.env.BOT_NAME,
   ownername: process.env.OWNER_NAME === undefined ? 'salmanytofficial' : process.env.OWNER_NAME,  
-  auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
+  auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? true : process.env.AUTO_READ_STATUS,
   autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
   antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD,
   alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
